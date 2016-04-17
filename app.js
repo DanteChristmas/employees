c91b41191e47ab0e533b68c5f2bd26a8103d2392
jjs5286 <jjs5286@gmail.com>
2016-04-16 21:02:39 -0600

    basic employee express jade mongo db app

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var express = require('express'),
+  routes = require('./routes'),
+  users = require('./routes/users'),
+  http = require('http'),
+  path = require('path');
+  
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+// app.use(favicon());
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -55,6 +51,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var EmployeeProvider = require('../employeeprovider').EmployeeProvider;
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+
+var employeeProvider = new EmployeeProvider('localhost', 27017);
+router.get('/employee/new', function(req, res) {
+  res.render('employee_new', {
+    title: 'New Employee'
+  });
+});
+
+router.post('/employee/new', urlencodedParser, function(req, res) {
+  employeeProvider.save({
+    title: req.body.title,
+    name: req.body.name
+  }, function(error, docs) {
+    res.redirect('/');
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  employeeProvider.findAll(function(error, emps) {
+    console.log(emps);
+    res.render('index', {
+      title: 'Employees',
+      employees: emps
+    });
+  });
 });
 
 module.exports = router;
